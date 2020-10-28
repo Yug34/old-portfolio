134def0aeaa210032cc184aa410eeea07b0ae28f
Yug34 <vertilax1337@gmail.com>
2020-10-29 01:27:16 +0530

    initial commit

---
+++ src/typed.js
@@ -0,0 +1,17 @@
+let name = new Typed("#typeName", {
+  strings: ["Yug"],
+  typeSpeed: 100,
+  showCursor: false,
+});
+
+let description = new Typed("#typeDescription", {
+  strings: ["Full-Stack Developer", "Machine Learning Engineer", "Research Volunteer"],
+  typeSpeed: 50,
+  startDelay: 500,
+  cursorChar: "|",
+  fadeOut: true,
+  fadeOutClass: 'typed-fade-out',
+  fadeOutDelay: 500,
+  loop: true,
+  loopCount: Infinity,
+});
---
+++ src/vantaWaves.js
@@ -0,0 +1,13 @@
+VANTA.WAVES({
+  el: "#home",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x0,
+  shininess: 29.00,
+  zoom: 1.4
+})
